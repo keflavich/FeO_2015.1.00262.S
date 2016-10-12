b8960ee0a02b6ed31e0fc370206bbd9329e93c85
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-10-12 16:59:00 -0600

    imaging script

--- reduction/script_band4/scriptForImaging_spws.py
+++ reduction/script_band4/scriptForImaging_spws.py
@@ -59,3 +59,33 @@ for spwnum in range(4,9):
         for suffix in ('pb', 'weight', 'sumwt', 'psf', 'model', 'mask',
                        'image', 'residual'):
             os.system('rm -rf {0}.{1}'.format(output, suffix))
+
+for robust in (-2, 0, 2):
+    output = "Band4_allspw_robust{0}_continuum".format(robust)
+    os.system('rm -rf ' + output + '*/')
+    tclean(vis=vis,
+           imagename=output,
+           field='OrionBullets',
+           spw='',
+           gridder='standard',
+           specmode='mfs',
+           veltype='radio',
+           outframe='LSRK',
+           interactive=F,
+           niter=1000,
+           imsize=[640,640],
+           cell='0.1arcsec',
+           weighting='briggs',
+           robust=robust,
+           phasecenter='',
+           threshold='10mJy',
+           savemodel='none',
+          )
+    myimagebase = output
+    impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.pb', outfile=myimagebase+'.image.pbcor', overwrite=True)
+    exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', overwrite=True, dropdeg=True)
+    exportfits(imagename=myimagebase+'.pb', fitsimage=myimagebase+'.pb.fits', overwrite=True, dropdeg=True)
+    exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', overwrite=True, dropdeg=True)
+    for suffix in ('pb', 'weight', 'sumwt', 'psf', 'model', 'mask',
+                   'image', 'residual'):
+        os.system('rm -rf {0}.{1}'.format(output, suffix))
